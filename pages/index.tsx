4bfd5d5f3891aaa6bb3d89a919b435ed26a4a0fe
Pablo Rodríguez <prodrigu75@gmail.com>
2022-04-21 00:30:29 -0400

    Changes main page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
-import { Typography } from '@mui/material';
-import { MainLayout } from 'components/layouts';
 import type { NextPage } from 'next';
+
+import { Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
+
+import { MainLayout } from 'components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <MainLayout title='Hello'>
-      <Typography variant='h1' color='primary'>Hello, World!</Typography>
+    <MainLayout title='Home'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 90px)' }}>
+            <CardHeader title="Pending" />
+            <CardContent>
+              {/* Add a new entry */}
+              {/* List all entries */}
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 90px)' }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 90px)' }}>
+            <CardHeader title="Finished" />
+          </Card>
+        </Grid>
+      </Grid>
     </MainLayout>
   );
 }
